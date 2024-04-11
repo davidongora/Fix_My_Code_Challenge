e38aa2ccafc9d74701187c30ae2bd774f0b9a7b7
DavidOngora <ongoradavid5.gmail.com>
2024-04-11 18:49:09 +0300

    made executable

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -3,6 +3,16 @@
 User class
 """
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/api/v1/status")
+def get_status():
+  """Returns API status."""
+  status_data = {"status": "OK", "message": "API is running"}
+  return status_data
+
 class User():
     """ Documentation """
 
@@ -24,6 +34,8 @@ class User():
    
     
 if __name__ == "__main__":
+#   app.run(debug=True)
+
 
     u = User()
     u.email = "john@snow.com"
